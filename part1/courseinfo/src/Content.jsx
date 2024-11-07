05020f2639b9468a0580abb29a052345581d6fb1
Juan Simancas <dev.juansimancas@proton.me>
2024-11-07 11:47:24 -0500

    added files for part1 exercises

---
+++ part1/courseinfo/src/Content.jsx
@@ -0,0 +1,14 @@
+import Part from "./Part";
+
+const Course = (props) => {
+  const parts = props.parts
+  return (
+    <>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
+    </>
+  )
+}
+
+export default Course;
---
+++ part1/courseinfo/src/Header.jsx
@@ -0,0 +1,8 @@
+const Header = (props) => {
+  const name = props.name
+  return (
+    <h1> {name} </h1>
+  )
+}
+
+export default Header;
