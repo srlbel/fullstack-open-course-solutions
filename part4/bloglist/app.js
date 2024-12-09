8e94bb47221d7056dd7e4985926df212c3bb4e0b
Juan Simancas <dev.juansimancas@proton.me>
2024-12-09 17:30:19 -0500

    bloglist exercise up to step 2
    currently there is a crash when trying to POST incorrent data. Pending fix

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,14 +1,28 @@
 const express = require('express')
-const cors = require('cors')
 const mongoose = require('mongoose')
+const cors = require('cors')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogRoutes = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 
 const app = express()
 
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MONGODB')
+  })
+  .catch(e => {
+    logger.error('error conecting to MONGODB: ', e.message)
+  })
+
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
+app.use('/api/blog', blogRoutes)
 
+app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
 
-module.exports = {
-  app
-}
+module.exports = app
