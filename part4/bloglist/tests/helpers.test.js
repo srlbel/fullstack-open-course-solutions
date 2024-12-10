15c85ba95d4cad6fcc912d3fc8363d3ea5271e9c
Juan Simancas <dev.juansimancas@proton.me>
2024-12-09 21:53:52 -0500

    Helper Functions and Unit Tests, step 2

--- part4/bloglist/tests/helpers.test.js
+++ part4/bloglist/tests/helpers.test.js
@@ -8,3 +8,53 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+    }
+  ]
+
+  const listWithManyBlogs = [
+    {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+    },
+    {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 15,
+    },
+    {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 20,
+    }
+  ]
+
+  const emptyList = []
+
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes(emptyList)
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listWithManyBlogs)
+    assert.strictEqual(result, 40)
+  })
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (blogList) => {
+  return blogList.reduce((sum, item) => sum + item.likes, 0);
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
