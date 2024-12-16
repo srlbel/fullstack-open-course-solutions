c46d09289ea335b9a07705c98bd4b1710b124eed
Juan Simancas <dev.juansimancas@proton.me>
2024-12-16 09:34:09 -0500

    update endpoints and requests for users and blogs

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,14 +1,17 @@
 const blogRoutes = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRoutes.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
 
   response.status(200).json(blogs)
 })
 
 blogRoutes.post('/', async (request, response) => {
   const body = request.body
+
+  const user = await User.findById(body.userId)
 
   if (!body.title || !body.url) {
     response.status(400).json({ error: 'Fields Missing' })
@@ -18,10 +21,15 @@ blogRoutes.post('/', async (request, response) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: user.id
   })
 
   const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
