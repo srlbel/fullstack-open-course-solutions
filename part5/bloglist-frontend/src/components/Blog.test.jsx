8cb4f2d7562c6f0d0c84f9514404624bda97fd3c
Juan Simancas <dev.juansimancas@proton.me>
2025-01-07 15:54:13 -0500

    add blog testing (wip)

---
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react'
+import Blog from './Blog'
+import { expect } from 'vitest'
+
+describe('<Blog />', () => {
+  test('renders content', () => {
+    const blog = {
+      title: 'Blog Test',
+      author: 'Author Test',
+      likes: 10,
+      url: 'http://url.com'
+    }
+
+    render(<Blog blog={blog} />)
+    screen.debug()
+
+    const element = screen.getByText('Blog Test - Author Test')
+    expect(element).toBeDefined()
+
+    // FIX: Url & Likes are being captured even when they're not displayed
+    const url = screen.queryByText('http://url.com')
+    const likes = screen.queryByText('likes 10')
+    expect(url).toBeNull()
+    expect(likes).toBeNull()
+  })
+
+})
