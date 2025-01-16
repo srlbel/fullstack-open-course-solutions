fd66993c7c65dc882f2f8ee237c1ce52ed23397a
Juan Simancas <dev.juansimancas@proton.me>
2025-01-16 10:39:21 -0500

    exercise 6.21

--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -2,3 +2,6 @@ import axios from 'axios'
 
 export const getAnecdotes = () =>
   axios.get('http://localhost:3001/anecdotes').then(res => res.data).catch(e => console.error(e))
+
+export const createAnecdote = (newAnecdote) =>
+  axios.post('http://localhost:3001/anecdotes', newAnecdote).then(res => res.data).catch(e => console.error(e))
