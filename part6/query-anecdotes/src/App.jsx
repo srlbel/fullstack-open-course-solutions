beb1b739485c34cef00a4830c050dbfe2119a3e0
Juan Simancas <dev.juansimancas@proton.me>
2025-01-16 10:18:13 -0500

    exercise 6.20

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,27 +1,31 @@
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './requests'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isLoading) return <div>loading data...</div>
+  if (result.isError) return <div>anecdote service not avaliable due to problems in server</div>
+
+  const anecdotes = result.data
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
-
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
+
       {anecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
