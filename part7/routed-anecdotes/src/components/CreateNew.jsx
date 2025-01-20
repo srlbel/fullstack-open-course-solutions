6ea8171bcb802b365f7e60393a3ab4f3ba44885b
Juan Simancas <dev.juansimancas@proton.me>
2025-01-20 10:52:43 -0500

    exercise 7.5

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -19,6 +19,13 @@ const CreateNew = (props) => {
     navigate('/')
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -36,6 +43,7 @@ const CreateNew = (props) => {
           <input {...info} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
