7892531cafca772d14eebbdb1585dd18b317ec2e
Juan Simancas <dev.juansimancas@proton.me>
2025-02-24 09:53:43 -0500

    exercise 9.19

--- part9/flight-diary/flight-diary-front/src/App.tsx
+++ part9/flight-diary/flight-diary-front/src/App.tsx
@@ -3,13 +3,24 @@ import diaryService from "./services/diaryService";
 import Content from "./components/Content";
 import { DiaryEntry, NewDiaryEntry } from "./types";
 import Form from "./components/Form";
+import axios from "axios";
 
 function App() {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [error, setError] = useState<string>("");
 
   const handleNewDiary = async (diary: NewDiaryEntry) => {
-    const response = await diaryService.newDiary(diary);
-    setDiaries(diaries.concat(response));
+    try {
+      const response = await diaryService.newDiary(diary);
+      setDiaries(diaries.concat(response));
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        setError(e.response?.data.message || e.response?.data || e.message);
+        setTimeout(() => setError(""), 5000);
+      } else {
+        console.error(e);
+      }
+    }
   };
 
   useEffect(() => {
@@ -27,6 +38,7 @@ function App() {
 
   return (
     <>
+      {error && <p style={{ color: "red" }}>{error}</p>}
       <Form addNewDiary={handleNewDiary} />
       <Content diaries={diaries} />
     </>
